7662d3f3d5194b27ce3fa6f027a0e7e1ee35f394
Veronica Preston <veronica.preston@bioscapedigital.com>
2022-06-26 12:26:39 -0400

    feat(package.json): adding commitizen and husky for linting and standard commits

--- Concepts/WebComponents/Simple/main.ts
+++ Concepts/WebComponents/Simple/main.ts
@@ -2,37 +2,42 @@ import { JSDOM } from 'jsdom';
 
 //mock DOM/headless browser
 const dom = new JSDOM(`<!DOCTYPE html><div id="app"></div>`);
-const {document, HTMLElement, HTMLDivElement, customElements} = dom.window;
+const { document, HTMLElement, HTMLDivElement, customElements } = dom.window;
 
- class A extends HTMLElement{
-     private _element:HTMLDivElement;
+class A extends HTMLElement {
+	private _element: HTMLDivElement;
 
-    constructor(){
-        super();
-        this._element = document.createElement('div');
+	constructor() {
+		super();
+		this._element = document.createElement('div');
 
-        this.attachShadow({ mode: 'open' });
-        this.shadowRoot!.appendChild(this._element);
-    }
-    
-    async connectedCallback() {
+		this.attachShadow({ mode: 'open' });
+		this.shadowRoot!.appendChild(this._element);
+	}
+
+	async connectedCallback() {
 		this.appendChild(this._element);
-	} 
-	
-
+	}
 }
 customElements.define('custom-div', A);
 /**
  * @class Main
  */
 class Main {
-   public container:HTMLDivElement;
-   public myDiv:A;
-    constructor(){
-      this.container = document.getElementById('app') as HTMLDivElement;
-      this.myDiv = document.createElement('custom-div') as A;
-      this.container!.appendChild(this.myDiv);
-      console.log(document.body.innerHTML);
-    }
+	public container: HTMLDivElement;
+	public myDiv: A;
+	constructor() {
+		this.container = document.getElementById('app') as HTMLDivElement;
+		this.myDiv = document.createElement('custom-div') as A;
+		this.container!.appendChild(this.myDiv);
+	}
+/**
+ * outputs to console
+ *
+ * @memberof Main
+ */
+output(): void {
+		console.log(document.body.innerHTML);
+	}
 }
 new Main();
